9862769e5b2a726f8247213fe5aedfb4acea3734
Javier <javelie@outlook.com>
2019-09-10 21:11:13 -0600

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by José Javier Cueto Mejía on 9/9/19.
+//  Copyright © 2019 José Javier Cueto Mejía. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        print(randomDiceIndex1)
+    }
+    
+}
+
